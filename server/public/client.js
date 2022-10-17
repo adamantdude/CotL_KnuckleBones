37b0626d5152d24d56c7d3e911f1328c62de7f05
adamantdude <adam.plee23@gmail.com>
2022-10-17 12:01:37 -0500

    add comments for readability and finalize game logic

--- server/public/client.js
+++ server/public/client.js
@@ -5,7 +5,7 @@ $(main);
 const MAX_ROW = 3;
 const MAX_COL = 3;
 
-const Dice = {
+const Dice = { // dice icons for rendering
     0:'',
     1:'⚀',
     2:'⚁',
@@ -15,16 +15,16 @@ const Dice = {
     6:'⚅'
 }
 
-let player = 1;
+let player = 1; // player turn ; alternates between 1 & 2
 
-let roll = 0;
+let roll = 0; // current roll ; starts at 0, randomized between 1 & 6 inclusive
 
-let totalPoints = {
+let totalPoints = { // for keeping track of total score
     '1': 0,
     '2': 0
 }
 
-const activeBoard = {
+const activeBoard = { // object state of the board
     '1': [
         [0, 0, 0],
         [0, 0, 0],
@@ -35,15 +35,16 @@ const activeBoard = {
         [0, 0, 0],
         [0, 0, 0],
     ],
-    reset: function () {
+    reset: function () { // reset state of the board
         this[1] = this[1].map(x => x = x.map(y => y = 0));
         this[2] = this[2].map(x => x = x.map(y => y = 0));
     }
 }
 
+// jq pointers for rendering to DOM
 let writableBoard;
-
 let boardPoints;
+let boardTotal;
 
 // game / DOM ready function
 function main() {
@@ -54,15 +55,16 @@ function main() {
 
     setJQPointers();
 
-    console.log(writableBoard['1']);
-    console.log(writableBoard['2']);
-    console.log($('.p2block'));
+    // console.log(writableBoard['1']);
+    // console.log(writableBoard['2']);
+    // console.log($('.p2block'));
+
     return 0;
 }
 
 function setJQPointers() {
 
-    writableBoard = {
+    writableBoard = { // player DOM boards
         '1': {
             '1': [$('.p1block#1-1'), $('.p1block#1-2'), $('.p1block#1-3')],
             '2': [$('.p1block#2-1'), $('.p1block#2-2'), $('.p1block#2-3')],
@@ -75,9 +77,14 @@ function setJQPointers() {
         }
     }
 
-    boardPoints = {
+    boardPoints = { // player DOM column scores
         '1': [$('#1.p1colScore'), $('#2.p1colScore'), $('#3.p1colScore')],
         '2': [$('#1.p2colScore'), $('#2.p2colScore'), $('#3.p2colScore')]
+    }
+
+    boardTotal = { // player DOM total score
+        '1': $('#p1Score'),
+        '2': $('#p2Score')
     }
 
 }
@@ -108,23 +115,32 @@ function beginGame() {
 function duringGame() {
     player = player == 1 ? 2 : 1; // change whose turn it is
     console.log('Player %d\'s turn!', player);
+    // turn off event listeners
     $('.p1colScore').off('click');
     $('.p2colScore').off('click');
+    // turn on only required event listeners for whose turn it is
     player == 1 ? $('.p1colScore').on('click', set) : $('.p2colScore').on('click', set);
+    // randomize the roll and render it
     roll = randomize(1, 6);
     rollRender();
     return;
 }
 
+// reset the game
 function resetGame() {
     console.log('Game Reset!');
+    // allow start to be pressed again after resetting
     $('#startBtn').prop('disabled', false);
     $('#resetBtn').prop('disabled', true);
+    // clear text for new game
     $('.p1block').text(''); $('.p2block').text('');
     $('.p1colScore').text('0'); $('.p2colScore').text('0');
+    $('.score').text('0');
+    // reset state of game
     activeBoard.reset();
 }
 
+// end of a game
 function endGame() {
     console.log('game ended! there\'s a winner!');
 
@@ -134,29 +150,29 @@ function endGame() {
 // set state function
 function set() {
     console.log($(this));
-    let column = $(this).attr('id')-1;
-    let attacker = activeBoard[player];
-    let set = false;
+    let column = $(this).attr('id')-1; // set dice in column of button pressed
+    let attacker = activeBoard[player]; // active player board ; whoever's turn it is
+    let set = false; // if (set)
     let row=0;
     for(; !set && row<attacker.length; ++row) {
-        if (!attacker[row][column]) {
+        if (!attacker[row][column]) { // if column is not full
             // set to the DOM
             writableBoard[player][row+1][column].append(Dice[roll]);
             // set to the STATE
             attacker[row][column] = roll;
-            set = true;
+            set = true; // if placed, set true
         }
     }
-    if(!set) return;
-    attack(column);
-    calculateScore();
+    if(!set) return; // if no dice placed, try again
+    attack(column); // attack other player board
+    calculateScore(); // calculate player scores
     return;
 }
 
 function attack(column) {
-    let defender = activeBoard[player == 1 ? 2 : 1]
+    let defender = activeBoard[player == 1 ? 2 : 1] // inactive player board ; whoever is waiting
     for(let row=0; row<defender.length; ++row) {
-        if(defender[row][column] == roll) {
+        if(defender[row][column] == roll) { // if the attacked column contains the current roll
             // reset the DOM
             writableBoard[player == 1 ? 2 : 1][row+1][column].text('');
             // reset the STATE
@@ -166,41 +182,64 @@ function attack(column) {
 }
 
 function calculateScore() {
-    let activePlayer = {
-        scores: [0, 0, 0],
-        boardFill: [0, 0, 0]
+    let scoresObj = {
+        scoresp1: [0, 0, 0],
+        scoresp2: [0, 0, 0],
+        boardFill1: [0, 0, 0],
+        boardFill2: [0, 0, 0]
     };
 
     let endGameCheck = false;
 
-    for(let i=0; i<MAX_ROW; ++i) {
-        let newObj = columnCount(i);
-        for(let num in newObj) {
-            if(num > 0 && newObj[num] > 0) {
-                activePlayer.scores[i] += addTotal(num, newObj[num]);
-                activePlayer.boardFill[i] += newObj[num];
+    for(let i=0; i<MAX_COL; ++i) { // for each column
+        let [newObj1, newObj2] = columnCount(i); // count the numbers and instances in a column for both boards
+        for(let num in newObj1) {
+            if(num > 0) { // for key is not '0'
+                // set the column totals into object with algorithm
+                scoresObj.scoresp1[i] += addTotal(num, newObj1[num]);
+                scoresObj.scoresp2[i] += addTotal(num, newObj2[num]);
+                // check for board spaces taken
+                scoresObj.boardFill1[i] += newObj1[num];
+                scoresObj.boardFill2[i] += newObj2[num];
             }
         }
-        boardPoints[player][i].text(activePlayer.scores[i]);
+        boardPoints[1][i].text(scoresObj.scoresp1[i]);
+        boardPoints[2][i].text(scoresObj.scoresp2[i]);
     }
 
-    totalPoints[player] = activePlayer.scores.reduce((x, y) => x + y);
+    // calculate total points for each player and render to DOM
+    totalPoints[1] = scoresObj.scoresp1.reduce((x, y) => x + y);
+    boardTotal[1].text(totalPoints[1]);
+    totalPoints[2] = scoresObj.scoresp2.reduce((x, y) => x + y);
+    boardTotal[2].text(totalPoints[2]);
 
-    if(activePlayer.boardFill.reduce((x, y) => x + y) == 9) endGameCheck = true;
+    // check for a filled board
+    let p1check = scoresObj.boardFill1.reduce((x, y) => x + y) == 9;
+    let p2check = scoresObj.boardFill2.reduce((x, y) => x + y) == 9;
 
+    if(p1check || p2check) endGameCheck = true;
+    // if a board is filled, end the game and declare a winner
     endGameCheck ? endGame() : duringGame();
 }
 
+// count numbers and instances for both boards of a certain column i
 function columnCount(i) {
-    let count = {
+    let count1 = {
+        '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0
+    };
+
+    let count2 = {
         '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0
     };
     
-    activeBoard[player].map(x => x[i]).map(y => count[y] ? count[y] += 1 : count[y] = 1);
+    // counts instances of numbers through a certain column
+    activeBoard[1].map(x => x[i]).map(y => count1[y] ? count1[y] += 1 : count1[y] = 1);
+    activeBoard[2].map(x => x[i]).map(y => count2[y] ? count2[y] += 1 : count2[y] = 1);
 
-    return count;
+    return [count1, count2];
 }
 
+// calculate score ; if 2 of same roll in one column: (roll + roll) * 2 ; etc
 function addTotal(num, instance) {
     let sum = 0;
     for(let i=0; i<instance; ++i) {
